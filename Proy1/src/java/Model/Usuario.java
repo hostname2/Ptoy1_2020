3d6f7fe92a18b200aaf76c9c511c5046c065cb63
Sebastian Grisanti <sgrisanti97@gmail.com>
2020-03-15 00:23:30 -0600

    ya usuaio noc que era lo otro

--- Proy1/src/java/Model/Usuario.java
+++ Proy1/src/java/Model/Usuario.java
@@ -10,5 +10,55 @@ package Model;
  * @author sebas
  */
 public class Usuario {
-    
+
+    public Usuario(String id_usuario, String clave_acceso, Integer clave_vencida, int rol) {
+        this.id_usuario = id_usuario;
+        this.clave_acceso = clave_acceso;
+        this.clave_vencida = clave_vencida;
+        this.rol = rol;
+    }
+
+    public Usuario() {
+        this.id_usuario = "";
+        this.clave_acceso = "";
+        this.clave_vencida = 0;
+        this.rol = 0;
+    }
+
+    public String getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(String id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getClave_acceso() {
+        return clave_acceso;
+    }
+
+    public void setClave_acceso(String clave_acceso) {
+        this.clave_acceso = clave_acceso;
+    }
+
+    public Integer getClave_vencida() {
+        return clave_vencida;
+    }
+
+    public void setClave_vencida(Integer clave_vencida) {
+        this.clave_vencida = clave_vencida;
+    }
+
+    public int getRol() {
+        return rol;
+    }
+
+    public void setRol(int rol) {
+        this.rol = rol;
+    }
+
+    String id_usuario;
+    String clave_acceso;
+    Integer clave_vencida;
+    int rol;
 }
