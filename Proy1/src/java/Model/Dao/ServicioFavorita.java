cacd8165c249f1b0487929dccca66f97009ec325
Sebastian Grisanti <sgrisanti97@gmail.com>
2020-04-11 19:13:12 -0600

    algunos cmabios y una clase servicio nueva

---
+++ Proy1/src/java/Model/Dao/ServicioFavorita.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model.Dao;
+
+import Model.Favorita;
+import Model.Moneda;
+import datos.BaseDatos;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ *
+ * @author sebas
+ */
+public class ServicioFavorita {
+
+    public Favorita obtenerFavorita(String cliente,String cuenta) {
+        Favorita r = null;
+        try (Connection cnx = obtenerConexion();
+                PreparedStatement stm = cnx.prepareStatement(CMD_RECUPERAR);) {
+            stm.clearParameters();
+            stm.setString(1, cliente);
+            stm.setString(2, cuenta);
+            try (ResultSet rs = stm.executeQuery()) {
+                if (rs.next()) {
+                    r = new Favorita(
+                            rs.getString("clinete_id_cliente"),
+                            rs.getString("cuenta_num_cuenta")
+                    );
+                }
+            }
+        } catch (IOException
+                | ClassNotFoundException
+                | IllegalAccessException
+                | InstantiationException
+                | SQLException ex) {
+            System.err.printf("Excepción: '%s'%n", ex.getMessage());
+        }
+        return r;
+    }
+
+    public List<Favorita> obtenerListaFavorita() {
+        List<Favorita> r = new ArrayList<>();
+        try (Connection cnx = obtenerConexion();
+                Statement stm = cnx.createStatement();
+                ResultSet rs = stm.executeQuery(CMD_LISTAR)) {
+            while (rs.next()) {
+                Favorita c = new Favorita(
+                            rs.getString("clinete_id_cliente"),
+                            rs.getString("cuenta_num_cuenta")
+                );
+                r.add(c);
+            }
+        } catch (IOException
+                | ClassNotFoundException
+                | IllegalAccessException
+                | InstantiationException
+                | SQLException ex) {
+            System.err.printf("Excepción: '%s'%n", ex.getMessage());
+        }
+        return r;
+    }
+
+    public void agregarFavorita(Favorita u) {
+
+        try (Connection cnx = obtenerConexion();
+                PreparedStatement stm = cnx.prepareStatement(CMD_AGREGAR)) {
+            stm.clearParameters();
+            stm.setString(1, u.getId_cliente());
+            stm.setString(2, u.getId_cuenta());
+            if (stm.executeUpdate() != 1) {
+                throw new Exception("Error no determinado");
+            }
+        } catch (Exception ex) {
+            System.err.printf("Excepción: '%s'%n", ex.getMessage());
+        }
+
+    }
+
+    public Connection obtenerConexion() throws
+            ClassNotFoundException,
+            IllegalAccessException,
+            InstantiationException,
+            IOException,
+            SQLException {
+        BaseDatos bd = BaseDatos.obtenerInstancia();
+        Properties cfg = bd.obtenerConfiguracion();
+        Connection cnx = bd.obtenerConexion(
+                cfg.getProperty("database"),
+                cfg.getProperty("user"),
+                cfg.getProperty("password")
+        );
+        return cnx;
+    }
+
+    public static void main(String[] args) {
+        ServicioMoneda su = new ServicioMoneda();
+        List<Moneda> usuarios = su.obtenerListaMonedas();
+        int i = 0;
+        for (Moneda u : usuarios) {
+            System.out.printf("%4d: %s,%n", ++i, u);
+        }
+    }
+
+    private static final String CMD_RECUPERAR
+            = "SELECT cliente_id_cliente, cuenta_num_cuenta FROM favorita WHERE cliente_id_cliente=? AND cuenta_num_cuenta=?; ";
+    private static final String CMD_LISTAR
+            = "SELECT cliente_id_cliente, cuenta_num_cuenta FROM favorita; ";
+    private static final String CMD_AGREGAR = "INSERT INTO favorita "
+            + "( cliente_id_cliente, cuenta_num_cuenta) "
+            + "VALUES(?, ?); ";
+}
--- Proy1/src/java/Model/Favorita.java
+++ Proy1/src/java/Model/Favorita.java
@@ -13,21 +13,32 @@ import java.util.List;
  */
 public class Favorita {
 
-    public Favorita(List<Cliente> lista_cliente, List<Cuenta> lista_cuenta) {
-        this.lista_cliente = lista_cliente;
-        this.lista_cuenta = lista_cuenta;
-    }
-
-    public Favorita() {
+    public Favorita(String id_cliente, String id_cuenta) {
+        this.id_cliente = id_cliente;
+        this.id_cuenta = id_cuenta;
         this.lista_cliente = null;
         this.lista_cuenta = null;
     }
-    
-    public void agregarCliente(Cliente c){
+
+    public Favorita(List<Cliente> lista_cliente, List<Cuenta> lista_cuenta) {
+        this.lista_cliente = lista_cliente;
+        this.lista_cuenta = lista_cuenta;
+        id_cliente = null;
+        id_cuenta = null;
+    }
+
+    public Favorita() {
+        id_cliente = null;
+        id_cuenta = null;
+        this.lista_cliente = null;
+        this.lista_cuenta = null;
+    }
+
+    public void agregarCliente(Cliente c) {
         lista_cliente.add(c);
     }
-    
-    public void agregarCuenta(Cuenta c){
+
+    public void agregarCuenta(Cuenta c) {
         lista_cuenta.add(c);
     }
 
@@ -46,12 +57,33 @@ public class Favorita {
     public void setLista_cuenta(List<Cuenta> lista_cuenta) {
         this.lista_cuenta = lista_cuenta;
     }
+
+    public String getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(String id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public String getId_cuenta() {
+        return id_cuenta;
+    }
+
+    public void setId_cuenta(String id_cuenta) {
+        this.id_cuenta = id_cuenta;
+    }
     
-    public String toString(){
-        return String.format("{{%s},{%s}}", getLista_cliente().toString(),getLista_cuenta().toString());
+    
+
+    public String toString() {
+        return String.format("{{%s},{%s}}", getLista_cliente().toString(), getLista_cuenta().toString());
     }
 
     List<Cliente> lista_cliente;
     List<Cuenta> lista_cuenta;
+
+    String id_cliente;
+    String id_cuenta;
 
 }
